50788f6a59fb74cb4ea80ac5b86573e54cdbd362
mmoottoo21 <86869673+mmoottoo21@users.noreply.github.com>
2021-11-28 23:42:31 +0300

    Add files via upload

---
+++ AndrIvan/ClassPram.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrIvan
+{
+    public class ClassPram
+    {
+        public static bool brake = false;
+        int x, y, shirina, visota;
+
+        public ClassPram(int x, int y, int shirina, int visota)
+        {
+            this.x = x;
+            this.y = y;
+            this.shirina = shirina;
+            this.visota = visota;
+        }
+        public void peremeshenie(int l, int h, ref int x, ref int y)
+        {
+            x -= l;
+            y -= h;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Перемещение удачно! {0} {1}", x, y);
+            Console.ResetColor();
+        }
+
+        public void sovmeshenie(int x1, int x2, int y1, int y2, int shirina1, int shirina2, int visota1, int visota2)
+        {
+
+            if (x1 > x2)
+            {
+                int buf = x1;
+                x1 = x2;
+                x2 = buf;
+            }
+            if (x1 + shirina1 < x2 + shirina2)
+            {
+                x = x1;
+                shirina = x2 - x1 + shirina2;
+            }
+            else
+            {
+                x = x1;
+                shirina = shirina2;
+            }
+            if (y1 > y2)
+            {
+                int buf = y1;
+                y1 = y2;
+                y2 = buf;
+            }
+            if (y1 + visota1 < y2 + visota2)
+            {
+                y = y1;
+                visota = y2 - y1 + visota2;
+            }
+            else
+            {
+                y = y1;
+                visota = visota2;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("x={0},y={1},Ширина={2},Высота={3}", x, y, shirina, visota);
+            Console.WriteLine("Совмещение удачно!");
+            Console.ResetColor();
+        }
+
+        public void izmenenie(int x, int y, ref int shirina, ref int visota)
+        {
+            shirina -= x;
+            visota -= y;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Изменение удачно! {0} {1}", shirina, visota);
+            Console.ResetColor();
+        }
+
+        public void show_Prim(int x, int y, int shirina, int visota)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0} {1} {2} {3}", x, y, shirina, visota);
+            Console.WriteLine("Вывод удачен!");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ AndrIvan/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrIvan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Построение по левой нижней точке, высоте и длине!");
+            Console.WriteLine("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину:");
+            int shirina = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите высоту:");
+            int visota = Convert.ToInt32(Console.ReadLine());
+            /////////////////////////////////////////////////
+            Console.WriteLine("Введите x2:");
+            int x2 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Введите y2:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину 2:");
+            int shirina2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите высоту 2:");
+            int visota2 = Convert.ToInt32(Console.ReadLine());
+            //////////////////////////////////////////////////////
+            ClassPram Func = new ClassPram(x, y, shirina, visota);
+            //На сколько перемещать и изменять.
+            int izmx = 1, izmy = 1;
+            int perx = 1, pery = 1;
+            //
+            Console.WriteLine("Введите комманманду меню:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Доступные команды: Перемещение, Изменение, Вывод, Совмешение, Выход");
+            Console.ResetColor();
+            do
+            {
+                switch (Console.ReadLine())
+                {
+                    case "Перемещение": Func.peremeshenie(perx, pery, ref x, ref y); break;
+                    case "Изменение": Func.izmenenie(izmx, izmy, ref shirina, ref visota); break;
+                    case "Вывод": Func.show_Prim(x, y, shirina, visota); break;
+                    case "Совмешение": Func.sovmeshenie(x, x2, y, y2, shirina, shirina2, visota, visota2); break;
+                    case "Выход": ClassPram.brake = true; break;
+                    default: Console.WriteLine("Команда не распознона!"); break;
+                }
+            } while (!ClassPram.brake);
+        }
+    }
+}
